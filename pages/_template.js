1494015e81c2da45ca0402657d0516cf42386fdd
Todd Baldwin <30380911+tbaldwin8@users.noreply.github.com>
2018-04-11 01:48:02 -0400

    Throwing up the landing page

--- pages/_template.js
+++ pages/_template.js
@@ -5,7 +5,18 @@ class Page extends React.Component {
     return (
         <div>
         <Head>
-        <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet"/>
+        <title>Ayobi</title>
+        <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800"/>
+        <link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <link rel='stylesheet' href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"/>
+        <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"/>
+        <link rel="stylesheet" href="/static/styles.css"/>
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css"/>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
         </Head>
         <div style={{fontFamily: 'Noto Sans, sans-serif', margin: 0, padding: 0}}>
         {this.props.children}
